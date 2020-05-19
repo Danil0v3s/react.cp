f180bf3a319ca99d5cea129395cc4f887b0b1419
Danilo Lemes <danilo.menezes@sky.uk>
2020-05-19 20:48:55 +0100

    remove php code

---
+++ pages/api/account/create.js
@@ -0,0 +1,13 @@
+export default async(req, res) => {
+    const {
+        serverGroupName,
+        username,
+        password,
+        passwordConfirm,
+        email,
+        emailConfirm,
+        gender,
+        birthDate,
+        code
+    } = req.body;
+}
